6c97d411ed6eb7559206c7f1ab827e613b6b182f
Sagleft <sagleft@inbox.ru>
2023-01-08 21:14:03 +0200

    add error message block

--- controller/public_html/js/navi.js
+++ controller/public_html/js/navi.js
@@ -15,13 +15,15 @@ $("#claimBonusBtn").click(function() {
 	$.post("/func/claim", {}, function (data) {
 		if(IsJsonString(data)) {
 			parsed_data = JSON.parse(data);
+			$("#requestVoucher").hide();
+
 			if(parsed_data.status == 'error') {
-				alert(parsed_data.error);
+				$("#errorMessage").show();
+				$("#errorInfo").html(parsed_data.error);
 			} else {
 				initConfetti();
 				render();
 				$("#voucherCode").val(parsed_data.data.code);
-				$("#requestVoucher").hide();
 				$("#voucherResultWrap").show();
 			}
 		} else {
